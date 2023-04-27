bc7453dc858026c5f61dd4b7958f2fa63b44ad1a
Rad <radoslav@everestkc.net>
2023-04-27 12:49:53 -0400

    Initial commit to dev branch

--- Tools/SetupGithubInfrastrucutreBaseline.ps1
+++ Tools/SetupGithubInfrastrucutreBaseline.ps1
@@ -10,7 +10,7 @@
 # Get the current folder name
 $folderName = (Get-Item -Path ".\").Name
 
-$env:GITHUB_TOKEN = "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"
+$env:GITHUB_TOKEN = "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"
 
 # GitHub API variables
 $repoName = $folderName
